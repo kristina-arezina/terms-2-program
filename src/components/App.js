4cc1833ee89853f724b1f6d348828b09a38c01eb
Kristina Arezina <arezina.kristina23@gmail.com>
2019-09-14 23:03:27 -0400

    added header

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, {Component, Fragment} from 'react';
+import Header from "./Layout/Header";
+
+export default class extends Component {
+    render(){
+        return <Fragment>
+            <Header />
+        </Fragment>
+    }
+}
+
+
---
+++ src/components/Layout/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div className="App">
+            <p>Header</p>
+        </div>
+    );
+}
+
+export default Header;
