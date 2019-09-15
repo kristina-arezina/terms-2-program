8d93f282327f8a998492bed48004cce7668c27e0
Kristina Arezina <arezina.kristina23@gmail.com>
2019-09-14 23:09:59 -0400

    made better way to import header and footer into app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, {Component, Fragment} from 'react';
-import Header from "./Layout/Header";
+import {Header, Footer} from "./Layout"
+
 
 export default class extends Component {
     render(){
         return <Fragment>
             <Header />
+            <Footer />
         </Fragment>
     }
 }
