af771e2746d2501844ede1aec6d7b503d5c3e4f2
Kristina Arezina <arezina.kristina23@gmail.com>
2019-09-15 07:33:30 -0400

    chnaged pane to diffrent components

--- src/components/Exercises/index.js
+++ src/components/Exercises/index.js
@@ -1,6 +1,18 @@
-import React from "react"
+import React from "react";
+import Grid from '@material-ui/core/Grid';
+import LeftPane from "./LeftPane";
+import RightPane from "./RightPane";
+
+const styles = {
+    Paper: { padding:20, marginTop:10, marginBottom:10, marginLeft:5}
+}
 
 export default props =>
-<div>
-    Hello from Exercises
-</div>
+<Grid container>
+    <Grid item sm>
+        <LeftPane styles = {styles}/>
+    </Grid>
+    <Grid item sm>
+        <RightPane styles = {styles}/>
+    </Grid>
+</Grid>
