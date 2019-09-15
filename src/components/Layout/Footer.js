be45087c03a643018ee55136c1fb0af221d5339e
Kristina Arezina <arezina.kristina23@gmail.com>
2019-09-14 23:31:55 -0400

    added tab in footer

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 
-function Footer() {
-    return (
-        <div className="App">
-            <p>Footer</p>
-        </div>
-    );
-}
 
-export default Footer;
+export default props =>
+    <Paper >
+        <Tabs
+            value={0}
+            indicatorColor="primary"
+            textColor="primary"
+            centered
+        >
+            <Tab label="Item One" />
+            <Tab label="Item Two" />
+            <Tab label="Item Three" />
+        </Tabs>
+    </Paper>
