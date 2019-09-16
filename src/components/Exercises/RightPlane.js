d56c8baeeb624b437810d31b01b53214e651e5f2
Kristina Arezina <arezina.kristina23@gmail.com>
2019-09-16 19:50:41 -0400

    never mind I did do that

--- src/components/Exercises/RightPlane.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Paper } from "@material-ui/core"
-
-
-export default ({styles}) =>
-    <Paper style={styles.Paper}>
-        Get Started Today
-    </Paper>
--- src/components/Exercises/index.js
+++ src/components/Exercises/index.js
@@ -1,20 +1,24 @@
 import React from "react"
-import { Grid } from "@material-ui/core"
-import LeftPlane from "./LeftPlane.js"
-import RightPlane from "./RightPlane.js"
+import { Grid, Paper } from "@material-ui/core"
+
 
 
 const styles = {
-    Paper: {padding:20, marginTop:10, marginBottom:10}
+    Paper: {padding:20, margin:10,}
 };
 
 export default props =>
     <Grid container>
         <Grid item sm>
-            <LeftPlane styles={styles}/>
+            <Paper style={styles.Paper}>
+                Left Pane
+        </Paper>
+
         </Grid>
     <Grid item sm>
-        <RightPlane styles={styles}/>
+        <Paper style={styles.Paper}>
+            Right Pane
+        </Paper>
 
     </Grid>
     </Grid>
