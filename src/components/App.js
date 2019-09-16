706bfd54c1990c6c6a62ed28687749c74c3d6b4f
Kristina Arezina <arezina.kristina23@gmail.com>
2019-09-16 13:07:04 -0400

    added store with objects that have exersies and muscle groups

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, {Component, Fragment} from 'react';
 import {Header, Footer} from "./Layout"
 import Exercises from "./Exercises"
+import {muscles, exercises} from "../store";
 
 
 export default class extends Component {
