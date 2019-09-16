7f82fef2edfa5b817817337837bb1514ca206f9c
Kristina Arezina <arezina.kristina23@gmail.com>
2019-09-15 23:09:27 -0400

    made ui nice ready for logic

--- src/components/Exercises/index.js
+++ src/components/Exercises/index.js
@@ -1,6 +1,20 @@
 import React from "react"
+import { Grid } from "@material-ui/core"
+import LeftPlane from "./LeftPlane.js"
+import RightPlane from "./RightPlane.js"
+
+
+const styles = {
+    Paper: {padding:20, marginTop:10, marginBottom:10}
+};
 
 export default props =>
-    <div>
-        Hello from Exercises
-    </div>
+    <Grid container>
+        <Grid item sm>
+            <LeftPlane styles={styles}/>
+        </Grid>
+    <Grid item sm>
+        <RightPlane styles={styles}/>
+
+    </Grid>
+    </Grid>
