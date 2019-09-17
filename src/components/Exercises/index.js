d0d78121298de873a8c03c48be736ce07cd62b2e
Kristina Arezina <arezina.kristina23@gmail.com>
2019-09-16 20:23:10 -0400

    got list to render on left side

--- src/components/Exercises/index.js
+++ src/components/Exercises/index.js
@@ -1,22 +1,39 @@
-import React from "react"
+import React, {Fragment} from "react"
 import { Grid, Paper, Typography } from "@material-ui/core"
 import {exercises} from "../../store";
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 
 
 
 const styles = {
-    Paper: {padding:20, margin:10,}
+    Paper: {padding:20, margin:10,},
+    Typography: {textTransform: "capitalize"}
 };
+
+function ListItemLink(props) {
+    return <ListItem button component="a" {...props} />;
+}
 
 export default ({exercises}) =>
     <Grid container>
         <Grid item sm>
             <Paper style={styles.Paper}>
                 {exercises.map(([group, exercises]) =>
-                    <Typography>
-                        {group}
-                    </Typography>
-
+                    <Fragment>
+                        <Typography >
+                            {group}
+                        </Typography>
+                        <List component="nav" aria-label="secondary mailbox folders">
+                            <ListItem button>
+                                <ListItemText primary="Trash" />
+                            </ListItem>
+                            <ListItemLink href="#simple-list">
+                                <ListItemText primary="Spam" />
+                            </ListItemLink>
+                        </List>
+                    </Fragment>
                 )}
 
             </Paper>
