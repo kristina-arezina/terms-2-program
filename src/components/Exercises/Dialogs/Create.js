9a80ac4d5141ee56f125a68d755d5f08eefa3a5a
Kristina Arezina <arezina.kristina23@gmail.com>
2019-09-17 22:53:42 -0400

    added create things

--- src/components/Exercises/Dialogs/Create.js
+++ src/components/Exercises/Dialogs/Create.js
@@ -1,6 +1,33 @@
-import react from "react"
+import React, {Fragment} from "react"
+import {Dialog, Button} from "@material-ui/core"
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import AddIcon from '@material-ui/icons/Add';
+
 
 export default props =>
-    <div>
+    <Fragment>
+        <Button varient="fab" color="primary" mini>
+            <AddIcon />
+        </Button>
 
-    </div>
+        <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+            <DialogTitle id="form-dialog-title">Create a New Exercise </DialogTitle>
+            <DialogContent>
+                <DialogContentText>
+                   Please fill out the form below.
+                </DialogContentText>
+                <form>
+
+                </form>
+
+            </DialogContent>
+            <DialogActions>
+                <Button color="primary">
+                    Create
+                </Button>
+            </DialogActions>
+        </Dialog>
+    </Fragment>
