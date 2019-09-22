315d3678c4b8f06f5d2a99f47a133a9f2a6fb6b6
Kristina Arezina <arezina.kristina23@gmail.com>
2019-09-22 18:55:49 -0400

    cleaned up code

--- src/components/Layout/JourneyList.js
+++ src/components/Layout/JourneyList.js
@@ -6,28 +6,18 @@ import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
-const useStyles = makeStyles(theme => ({
-    root: {
-        width: '100%',
-    },
-    heading: {
-        fontSize: theme.typography.pxToRem(15),
-        fontWeight: theme.typography.fontWeightRegular,
-    },
-}));
 
 export default function JourneyList() {
-    const classes = useStyles();
 
     return (
-        <div className={classes.root}>
+        <div>
             <ExpansionPanel>
                 <ExpansionPanelSummary
                     expandIcon={<ExpandMoreIcon />}
                     aria-controls="panel1a-content"
                     id="panel1a-header"
                 >
-                    <Typography className={classes.heading}>Expansion Panel 1</Typography>
+                    <Typography>Complete Beginner</Typography>
                 </ExpansionPanelSummary>
                 <ExpansionPanelDetails>
                     <Typography>
