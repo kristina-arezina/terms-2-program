34d4e7314f8b205eb651aaf9a8c2039ca561efff
Kristina Arezina <arezina.kristina23@gmail.com>
2019-10-04 07:53:54 -0400

    added hamburger icon on toolbar

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -3,6 +3,10 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+
 
 export class Header extends Component {
 
@@ -32,6 +36,9 @@ export class Header extends Component {
         }}>
         <AppBar color="#f4f4f4">
             <Toolbar>
+                <IconButton edge="start" color="inherit" aria-label="menu">
+                    <MenuIcon />
+                </IconButton>
                 <Typography variant="h6" style={{flexGrow: 1}}>
                     Learn2Program
                 </Typography>
