4cac4bb74098af55830987fa246725bd49a0ead9
Kristina Arezina <arezina.kristina23@gmail.com>
2019-10-03 22:01:08 -0400

    added journeyStore

--- src/journeyStore.js
+++ src/journeyStore.js
@@ -1,8 +1,85 @@
-// export const journeyCB = [
-//     {
-//         description: "Civil engineer turned programmer that loves to problem solve. Believes you learn to program " +
-//             "though the Learn and implement pattern. Believes in consistency. You can learn most of the awesome things " +
-//             "about programming by reading others code. You should read other’s open-source code. "
-//         link: "https://medium.com/mindorks/a-self-taught-programmers-journey-6899cff1aef0\n"
-//     }
-// ]
+export const level = [
+    "complete beginner", "react", "C++", "JavaScript"
+]
+
+export const link = [
+    "https://medium.com/mindorks/a-self-taught-programmers-journey-6899cff1aef0"
+]
+
+export const journeys = [
+    {
+        id: 'CB1',
+        title: 'A Self Taught Programmer’s Journey',
+        description: 'Civil engineer turned programmer that loves to problem solve. Believes you learn to program ' +
+            'though the “Learn and Implement” pattern. Believes in consistency. You can learn most of the awesome ' +
+            'things about programming by reading others code. You should read other’s open-source code.',
+        link: 'https://medium.com/mindorks/a-self-taught-programmers-journey-6899cff1aef0',
+        level:"complete beginner"
+    },
+    {
+        id: 'CB2',
+        title: 'What I’ve Learned from a year of coding',
+        description: 'In high school learned about graphic design. Was programing for one year. ' +
+            'Preaches of starting small “nobody can expect to understand coding overnight”. ' +
+            'Learned HTML and CSS first. Liked freecodecamp. Recommends to start a programming project ' +
+            'when you feel comfortable with the syntax of  language.',
+        link: 'https://www.freecodecamp.org/news/what-ive-learned-from-a-year-of-coding-bf39c5823e9b/',
+        level:"complete beginner"
+    },
+    {
+        id: 'CB3',
+        title: 'Teach Yourself Programming in Ten Years',
+        description: 'This website looks a bit sketchy but if you ignore that it provides invaluable ' +
+            'information about why you should learn to program correctly. The claim made is that you can learn the ' +
+            'basics of programming quickly however, researchers have shown it takes about ten years to develop ' +
+            'expertise in a particular area. Believes you must challenge yourself with a task that is just beyond your ' +
+            'current ability, trying it, analyzing your performance while and after doing it, and correcting any ' +
+            'mistakes. Make programming fun. Learn by doing.',
+        link: 'https://www.norvig.com/21-days.html',
+        level:"complete beginner"
+    },
+    {
+        id: 'React1',
+        title: 'Hello',
+        description: 'Civil engineer turned programmer that loves to problem solve. Believes you learn to program ' +
+            'though the “Learn and Implement” pattern. Believes in consistency. You can learn most of the awesome ' +
+            'things about programming by reading others code. You should read other’s open-source code.',
+        link: 'https://medium.com/mindorks/a-self-taught-programmers-journey-6899cff1aef0',
+        level:"react"
+    },
+    {
+        id: 'React2',
+        title: 'What I’ve Learned from a year of coding',
+        description: 'In high school learned about graphic design. Was programing for one year. ' +
+            'Preaches of starting small “nobody can expect to understand coding overnight”. ' +
+            'Learned HTML and CSS first. Liked freecodecamp. Recommends to start a programming project ' +
+            'when you feel comfortable with the syntax of  language.',
+        link: 'https://www.freecodecamp.org/news/what-ive-learned-from-a-year-of-coding-bf39c5823e9b/',
+        level:"react"
+    },
+    {
+        id: 'React3',
+        title: 'Teach Yourself Programming in Ten Years',
+        description: 'This website looks a bit sketchy but if you ignore that it provides invaluable ' +
+            'information about why you should learn to program correctly. The claim made is that you can learn the ' +
+            'basics of programming quickly however, researchers have shown it takes about ten years to develop ' +
+            'expertise in a particular area. Believes you must challenge yourself with a task that is just beyond your ' +
+            'current ability, trying it, analyzing your performance while and after doing it, and correcting any ' +
+            'mistakes. Make programming fun. Learn by doing.',
+        link: 'https://www.norvig.com/21-days.html',
+        level:"react"
+    },
+    {
+        id: 'C++1',
+        title: 'Testing',
+        description: 'This website looks a bit sketchy but if you ignore that it provides invaluable ' +
+            'information about why you should learn to program correctly. The claim made is that you can learn the ' +
+            'basics of programming quickly however, researchers have shown it takes about ten years to develop ' +
+            'expertise in a particular area. Believes you must challenge yourself with a task that is just beyond your ' +
+            'current ability, trying it, analyzing your performance while and after doing it, and correcting any ' +
+            'mistakes. Make programming fun. Learn by doing.',
+        link: 'https://www.norvig.com/21-days.html',
+        level:"C++"
+    },
+
+]
