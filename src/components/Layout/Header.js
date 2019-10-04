3226b7e378f36760a74cfab659680d15ab6f7b50
Kristina Arezina <arezina.kristina23@gmail.com>
2019-10-04 15:40:04 -0400

    added side bar navigation button

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -3,8 +3,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import TemporaryDrawer from "./SmallNavBar"
 
 
 
@@ -36,9 +35,7 @@ export class Header extends Component {
         }}>
         <AppBar color="#f4f4f4">
             <Toolbar>
-                <IconButton edge="start" color="inherit" aria-label="menu">
-                    <MenuIcon />
-                </IconButton>
+                <TemporaryDrawer/>
                 <Typography variant="h6" style={{flexGrow: 1}}>
                     Learn2Program
                 </Typography>
