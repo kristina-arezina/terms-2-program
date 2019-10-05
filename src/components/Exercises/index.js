0f36fbc603d750612495e13ff5ccadc3b1954fe7
Kristina Arezina <arezina.kristina23@gmail.com>
2019-10-05 10:00:50 -0400

    made it responsive

--- src/components/Exercises/index.js
+++ src/components/Exercises/index.js
@@ -7,6 +7,7 @@ import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
 import Link from '@material-ui/core/Link';
+import MediaQuery from 'react-responsive';
 
 const styles = {
     Paper: {
@@ -35,75 +36,150 @@ export default ({
      },
     onDelete
 }) =>
-    <Grid container>
-        <Grid item xs={12} sm={6}>
-            <Paper style={styles.Paper} >
-                {exercises.map(([group, exercises]) =>
-                    !category || category === group
-                    ? <Fragment key={group}>
-                            <Typography variant={"h6"}
-                                        style={{
-                                            textTransform: 'uppercase',
-                                            color:"#00867d",
-                                            fontFamily:"courier new",
-                                        }}
-                            >
-                                {group}
-                            </Typography>
-                            <List component="ul" >
-                                {exercises.map(({id, title}) =>
-                                    <ListItem
-                                        key={id}
-                                        button
-                                        onClick={() => onSelect(id)}
+    <MediaQuery query="(max-width: 758px)">
+        {matches =>
+            matches ? (<Grid container>
+                <Grid item xs={12} >
+                    <Paper style={styles.Paper} >
+                        {exercises.map(([group, exercises]) =>
+                            !category || category === group
+                                ? <Fragment key={group}>
+                                    <Typography variant={"h6"}
+                                                style={{
+                                                    textTransform: 'uppercase',
+                                                    color:"#00867d",
+                                                    fontFamily:"courier new",
+                                                }}
                                     >
-                                        <ListItemText
-                                         primary={title}
-                                         style={{
-                                             fontFamily:"century gothic",
-                                         }}/>
-                                        <ListItemSecondaryAction>
-                                            <IconButton onClick={() => onDelete(id)}>
-                                                <DeleteIcon/>
-                                            </IconButton>
-                                        </ListItemSecondaryAction>
-                                    </ListItem>
-                                )}
-                            </List>
-                        </Fragment>
-                        : null
-                )}
-            </Paper>
+                                        {group}
+                                    </Typography>
+                                    <List component="ul" >
+                                        {exercises.map(({id, title}) =>
+                                            <ListItem
+                                                key={id}
+                                                button
+                                                onClick={() => onSelect(id)}
+                                            >
+                                                <ListItemText
+                                                    primary={title}
+                                                    style={{
+                                                        fontFamily:"century gothic",
+                                                    }}/>
+                                                <ListItemSecondaryAction>
+                                                    <IconButton onClick={() => onDelete(id)}>
+                                                        <DeleteIcon/>
+                                                    </IconButton>
+                                                </ListItemSecondaryAction>
+                                            </ListItem>
+                                        )}
+                                    </List>
+                                </Fragment>
+                                : null
+                        )}
+                    </Paper>
 
-        </Grid>
-    <Grid item xs={12} sm={6}>
-        <Paper style={styles.Paper}>
+                </Grid>
+                <Grid item xs={12} >
+                    <Paper style={styles.Paper}>
 
-            <Typography variant="h3"style={{ fontFamily:"century gothic",}}>
-                {title}
-            </Typography>
-            <Typography variant={"body1"} style={{marginTop:20, fontFamily:"century gothic",}}>
-                {description}
-                <br/>
-                <br/>
-                {description2}
-                {resourse}
-                <br/>
-                    <ul>
-                        <li>
-                           <Link rel="noopener" variant="body1" href={link} target="_blank">
-                            {link}
-                            </Link>
-                        </li>
-                        <li>
-                            <Link rel="noopener" variant="body1" href={video} target="_blank">
-                                {video}
-                            </Link>
-                        </li>
-                    </ul>
-            </Typography>
+                        <Typography variant="h3"style={{ fontFamily:"century gothic",}}>
+                            {title}
+                        </Typography>
+                        <Typography variant={"body1"} style={{marginTop:20, fontFamily:"century gothic",}}>
+                            {description}
+                            <br/>
+                            <br/>
+                            {description2}
+                            {resourse}
+                            <br/>
+                            <ul>
+                                <li>
+                                    <Link rel="noopener" variant="body1" href={link} target="_blank">
+                                        {link}
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link rel="noopener" variant="body1" href={video} target="_blank">
+                                        {video}
+                                    </Link>
+                                </li>
+                            </ul>
+                        </Typography>
 
-        </Paper>
+                    </Paper>
 
-    </Grid>
-    </Grid>
+                </Grid>
+            </Grid>): (
+                <Grid container>
+                <Grid item sm={6}>
+                    <Paper style={styles.Paper} >
+                        {exercises.map(([group, exercises]) =>
+                            !category || category === group
+                                ? <Fragment key={group}>
+                                    <Typography variant={"h6"}
+                                                style={{
+                                                    textTransform: 'uppercase',
+                                                    color:"#00867d",
+                                                    fontFamily:"courier new",
+                                                }}
+                                    >
+                                        {group}
+                                    </Typography>
+                                    <List component="ul" >
+                                        {exercises.map(({id, title}) =>
+                                            <ListItem
+                                                key={id}
+                                                button
+                                                onClick={() => onSelect(id)}
+                                            >
+                                                <ListItemText
+                                                    primary={title}
+                                                    style={{
+                                                        fontFamily:"century gothic",
+                                                    }}/>
+                                                <ListItemSecondaryAction>
+                                                    <IconButton onClick={() => onDelete(id)}>
+                                                        <DeleteIcon/>
+                                                    </IconButton>
+                                                </ListItemSecondaryAction>
+                                            </ListItem>
+                                        )}
+                                    </List>
+                                </Fragment>
+                                : null
+                        )}
+                    </Paper>
+
+                </Grid>
+                <Grid item sm={6}>
+                    <Paper style={styles.Paper}>
+
+                        <Typography variant="h3"style={{ fontFamily:"century gothic",}}>
+                            {title}
+                        </Typography>
+                        <Typography variant={"body1"} style={{marginTop:20, fontFamily:"century gothic",}}>
+                            {description}
+                            <br/>
+                            <br/>
+                            {description2}
+                            {resourse}
+                            <br/>
+                            <ul>
+                                <li>
+                                    <Link rel="noopener" variant="body1" href={link} target="_blank">
+                                        {link}
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link rel="noopener" variant="body1" href={video} target="_blank">
+                                        {video}
+                                    </Link>
+                                </li>
+                            </ul>
+                        </Typography>
+
+                    </Paper>
+
+                </Grid>
+            </Grid>) }</MediaQuery>
+
