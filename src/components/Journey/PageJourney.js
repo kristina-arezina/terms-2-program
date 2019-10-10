f374e0ec42c5cd681dd96e19323a84db88f41d4b
Kristina Arezina <arezina.kristina23@gmail.com>
2019-10-10 18:02:55 -0400

    still works

--- src/components/Journey/PageJourney.js
+++ src/components/Journey/PageJourney.js
@@ -4,21 +4,21 @@ import Typography from '@material-ui/core/Typography';
 
 export class PageJourney extends Component {
     render(){
-return (
-    <Paper >
-        <Typography variant="h5" component="h3" style={{
-            paddingTop:150,
-            paddingBottom:80,
-            textAlign:"center",
-            textTransform: 'uppercase',
-            backgroundColor:"#f4f4f4",
-            fontFamily:"courier new",
-        }}>
-        Programing Journeys
-        </Typography>
 
-    </Paper>
-)
+        return (
+            <Paper>
+                <Typography variant="h5" component="h3" style={{
+                    paddingTop:150,
+                    paddingBottom:80,
+                    textAlign:"center",
+                    textTransform: 'uppercase',
+                    backgroundColor:"#f4f4f4",
+                    fontFamily:"courier new",
+                }}>
+                    Programing Journeys
+                </Typography>
+            </Paper>
+        )
     }
 }
 
