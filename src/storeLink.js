c6396b7da03817471bb45c38bba0e20462487298
Kristina Arezina <arezina.kristina23@gmail.com>
2019-10-09 20:41:08 -0400

    have list of links

---
+++ src/storeLink.js
@@ -0,0 +1,29 @@
+export const titles = [
+    'browsers', 'operating system', 'software development','terminal', 'programing languages'
+]
+
+export const links = [
+    {
+        title: 'browsers',
+        link1:'https://www.lifewire.com/what-is-a-browser-446234'
+        link2:"https://www.youtube.com/watch?v=BrXPcaRlBqo",
+        link3:"https://www.youtube.com/watch?v=BrXPcaRlBqo",
+        link4:"https://www.youtube.com/watch?v=BrXPcaRlBqo",
+        video1:"https://www.youtube.com/watch?v=BrXPcaRlBqo",
+        video2:"https://www.youtube.com/watch?v=BrXPcaRlBqo",
+        video3:"https://www.youtube.com/watch?v=BrXPcaRlBqo",
+        video4:"https://www.youtube.com/watch?v=BrXPcaRlBqo",
+    },
+    {
+        title: 'operating system',
+        link1:'https://edu.gcfglobal.org/en/computerbasics/understanding-operating-systems/1/',
+        link2:"https://edu.gcfglobal.org/en/computerbasics/understanding-operating-systems/1/",
+        link3:"https://edu.gcfglobal.org/en/computerbasics/understanding-operating-systems/1/",
+        link4:"https://edu.gcfglobal.org/en/computerbasics/understanding-operating-systems/1/",
+        video1:"https://www.youtube.com/watch?v=pTdSs8kQqSA",
+        video2:"https://www.youtube.com/watch?v=pTdSs8kQqSA",
+        video3:"https://www.youtube.com/watch?v=pTdSs8kQqSA",
+        video4:"https://www.youtube.com/watch?v=pTdSs8kQqSA",
+    }
+
+]
