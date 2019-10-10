4b76703eabd0ac1ed499c03eb5f0b370ced63c5f
Kristina Arezina <arezina.kristina23@gmail.com>
2019-10-10 18:07:50 -0400

    still works

--- src/components/Journey/JourneyList.js
+++ src/components/Journey/JourneyList.js
@@ -12,7 +12,7 @@ import Link from '@material-ui/core/Link';
 
 export default ({journeys}) =>
     <Paper>
-        {journeys.map(([group,journeys])  =>
+        {journeys.map(([group,journeys]) =>
             <ExpansionPanel>
                 <ExpansionPanelSummary
                     expandIcon={<ExpandMoreIcon/>}
@@ -27,45 +27,48 @@ export default ({journeys}) =>
 
                 <ExpansionPanelDetails>
 
-                    <List >{journeys.map(({ title, description, link }) =>
-                        <ListItem alignItems="flex-start"  >
-                            <Paper style={{backgroundColor:"#e0f2f1", padding:10}}
-                            >
+                    <List>
+                        {journeys.map(({ title, description, link }) =>
+                            <ListItem alignItems="flex-start"  >
+                                <Paper style={{backgroundColor:"#e0f2f1", padding:10}}
+                                >
 
-                            <ListItemText
-                                primary={
-                                    <Typography
-                                    style={{marginBottom:"1.5%",fontFamily:"century gothic", color:"#000000" }}
-                                    >
-                                        {title}
-                                    </Typography>
-                                }
-                                secondary={
-                                    <React.Fragment>
-                                        <Typography
-                                            style={{fontFamily:"century gothic", color:"#000000",fontWeight: 50, margin:20}}
-                                        >
-                                            {description}
-                                            <br/>
-                                            <br/>
-                                            <Link rel="noopener" variant="body1" href={link} target="_blank">
-                                                {link}
-                                            </Link>
-                                        </Typography>
-                                    </React.Fragment>
-
-                                }
-                            /></Paper>
-
-
-                        </ListItem>
-
-
-                    )}
-
+                                    <ListItemText
+                                        primary={
+                                            <Typography
+                                                style={{
+                                                    marginBottom:"1.5%",
+                                                    fontFamily:"century gothic",
+                                                    color:"#000000"
+                                                }}
+                                            >
+                                                {title}
+                                            </Typography>
+                                        }
+                                        secondary={
+                                            <React.Fragment>
+                                                <Typography
+                                                    style={{fontFamily:"century gothic",
+                                                        color:"#000000",
+                                                        fontWeight: 50,
+                                                        margin:20
+                                                    }}
+                                                >
+                                                    {description}
+                                                        <br/>
+                                                        <br/>
+                                                    <Link rel="noopener" variant="body1" href={link} target="_blank">
+                                                        {link}
+                                                    </Link>
+                                                </Typography>
+                                            </React.Fragment>
+                                        }
+                                    />
+                                </Paper>
+                            </ListItem>
+                        )}
                     </List>
                 </ExpansionPanelDetails>
-
             </ExpansionPanel>
         )}
     </Paper>
