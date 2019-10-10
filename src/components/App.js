e7100d0b78398a2888d5d6d2397f49b19ff14daa
Kristina Arezina <arezina.kristina23@gmail.com>
2019-10-09 20:33:40 -0400

    got rid of useless code in app.js

--- src/components/App.js
+++ src/components/App.js
@@ -13,37 +13,9 @@ export default class extends Component {
         exercise: {},
     }
 
-    getExercisesByMuscles(){
-        return Object.entries(
-            this.state.exercises.reduce((exercises, exercise) => {
-            const {muscles} = exercise
-
-            exercises[muscles] = exercises[muscles]
-            ? [...exercises[muscles], exercise]
-                : [exercise]
-
-            return exercises
-        }, {})
-        )
-    }
-
-    handleCategorySelect = catergory => {
-        this.setState({
-            catergory
-        })
-    }
-
-    handleExerciseSelect = id => {
-        this.setState(({exercises}) => ({
-            exercise: exercises. find(ex => ex.id ===id)
-            }))
-    }
-
 
 
     render() {
-        const exercises = this.getExercisesByMuscles(),
-            {catergory, exercise} = this.state
 
         return (
                 <Router>
