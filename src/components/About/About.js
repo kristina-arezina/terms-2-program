d5bb6ac3833127caed09215c17f8b675710bc6fa
Kristina Arezina <arezina.kristina23@gmail.com>
2019-10-10 19:52:18 -0400

    fixing

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,14 +5,12 @@ import Paper from '@material-ui/core/Paper';
 import MediaQuery from 'react-responsive';
 import {Header} from "../Layout/HeaderFooter.js"
 
-
-
 function About(){
     return(
         <div>
             <Header/>
             <AboutHeader/>
-            <MediaQuery query="(max-width: 841px)">
+                <MediaQuery query="(max-width: 841px)">
                 {matches =>
                     matches ? (
                         <div className="parent">
@@ -25,51 +23,60 @@ function About(){
                                             backgroundColor:"white",
                                             color:"#000000",
                                             margin:20
-                                        }}
+                                            }}
                                     >
-                                        <h1 style={{
+                                        <h1
+                                            style={{
                                             paddingTop: 10,
                                             paddingBottom: 5,
                                             border:1,
                                             fontFamily:"courier new",
                                             fontWeight: "normal"
-                                        }}
-                                        >You Can Program</h1>
-                                        <p style={{
-                                            fontFamily:"century gothic",
-                                            fontSize:16,
-                                            lineHeight: 1.6
-                                        }}
+                                            }}
+                                            >You Can Program
+                                        </h1>
+                                        <p
+                                            style={{
+                                                fontFamily:"century gothic",
+                                                fontSize:16,
+                                                lineHeight: 1.6
+                                            }}
                                         >
                                             Programing is not difficult but it is complicated. Something is difficult if it requires a lot of skills to accomplish that task. For example, memorizing a huge list of random numbers in 1 minute is difficult as not many people have the capacity to do that. Thankfully, programming is complicated rather than difficult which means anyone can learn to program if they get over the two main barriers that make programming seem difficult.<br/><br/>
                                             Many people wish to learn how to program since it is a powerful tool that allows for innovation, creation, and optimization. Unfortunately, getting started is intimidating. A major barrier that gets in the way of learning programming is the language that is used in this field. There seem to be hundreds of terms, abbreviations, and acronyms for concepts that seem foreign. Being unable to decipher the words in every programing article or video tutorial is discouraging to many and causes people to give up on learning to program.<br/><br/>
                                             Another major setback for people that wish to get into programming is that it is very hard to figure out where to start. Hundreds of programming languages exist not to mention the thousands of libraries and frameworks that go with each language. Additionally, each language uses different rules for writing programs (called syntax) and is capable of doing specific jobs well. It is overwhelming to attempt to learn the different methods of programming and makes people stop their journey before they have even started.
-
                                         </p>
                                     </Paper>
                                 </Grid>
 
 
                                 <Grid item xs={12}>
-                                    <Paper style={{
+                                    <Paper
+                                        style={{
                                         padding: 20,
                                         textAlign: 'left',
                                         backgroundColor:"white",
                                         color:"#000000",
                                         margin:20
-                                    }}
+                                        }}
                                     >
-                                        <h1 style={{
+                                        <h1
+                                            style={{
                                             paddingTop: 10,
                                             paddingBottom: 5,
                                             border:1,
                                             fontFamily:"courier new",
                                             fontWeight: "normal"
-                                        }}>Get Started Today</h1>
-                                        <p style={{
-                                            fontFamily:"century gothic", fontSize:16, lineHeight: 1.6
+                                            }}
+                                        >Get Started Today
+                                        </h1>
+                                        <p
+                                            style={{
+                                            fontFamily:"century gothic",
+                                                fontSize:16,
+                                                lineHeight: 1.6
 
-                                        }}
+                                            }}
                                         >
                                             Learn2Program is a web app designed to encourage people to program. The core belief of this application is that the world is missing out on amazing innovation and problem-solving because people are misguided into thinking programing is beyond their intellectual abilities.<br/><br/>
 
@@ -77,13 +84,12 @@ function About(){
                                             language / library / framework, what it can be used for, along with a list of resources that can help you get started learning it.<br/><br/>
 
                                             The second part of Learn2Program is meant to motivate and inspire people to program at all levels. The best way to learn any skill that is new to you is to see how successful people went about learning it; as the old saying goes, "If you can't beat them, join them". The PROGRAMING JOURNEYS page, as suggested by its name, presents a list of programing journeys from other people. In addition to helping complete beginners get started the PROGRAMING JOURNEYS page contains stories that explain how people approached learning specific skills in programming (languages / libraries / frameworks).
-
                                         </p>
                                     </Paper>
                                 </Grid>
                             </Grid>
                         </div>
-                                ) : (
+                ):(
                         <div className="parent">
                             <Grid container spacing={1}  >
                                 <Grid  item xs={6}>
@@ -145,10 +151,10 @@ function About(){
                                 </Grid>
                             </Grid>
                         </div>
-                    )
-                    }
-                                </MediaQuery>
-        </div>)
+                )}
+                </MediaQuery>
+        </div>
+    )
 }
 
 
