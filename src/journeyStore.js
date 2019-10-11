dbb6385c67894c450bb4975900098dee91089a58
Kristina Arezina <arezina.kristina23@gmail.com>
2019-10-11 07:25:16 -0400

    added programing journies for complete beginners

--- src/journeyStore.js
+++ src/journeyStore.js
@@ -6,20 +6,22 @@ export const journeys = [
     {
         id: 'CB1',
         title: 'A Self Taught Programmer’s Journey',
-        description: 'Civil engineer turned programmer that loves to problem solve. Believes you learn to program ' +
-            'though the “Learn and Implement” pattern. Believes in consistency. You can learn most of the awesome ' +
-            'things about programming by reading others code. You should read other’s open-source code.',
+        description: "In this article, a civil engineer Amit Shekhar, explains how he changed career paths by learning " +
+            "to program on his own time. Amit was drawn to programming as he loves to problem solve. Some of his advice " +
+            "for beginner programmers is to learn through the “Learn and Implement” pattern and to stay consistent with " +
+            "programming. Also, he thinks one of the best ways to learn programming is by reading other people's code.",
         link: 'https://medium.com/mindorks/a-self-taught-programmers-journey-6899cff1aef0',
         level:"complete beginner"
     },
     {
         id: 'CB2',
         title: 'What I’ve Learned from a year of coding',
-        description: 'In high school learned about graphic design. Was programing for one year. ' +
-            'Preaches of starting small “nobody can expect to understand coding overnight”. ' +
-            'Learned HTML and CSS first. Liked freecodecamp. Recommends to start a programming project ' +
-            'when you feel comfortable with the syntax of  language.',
-        link: 'https://www.freecodecamp.org/news/what-ive-learned-from-a-year-of-coding-bf39c5823e9b/',
+        description: "Charles Scheuer was programing for one year and is amazed at what he learned autonomously for that " +
+            "short period of time. He encourages new programmers to start small and simple as “nobody can expect to " +
+            "understand coding overnight”. Charles learned HTML and CSS first and recommends FreeCodeCamp.org to learn. " +
+            "Additionally, he recommends starting a programming project once you feel comfortable with the syntax of a" +
+            " programming language as it is one of the most efficient ways to learn.",
+    link: 'https://www.freecodecamp.org/news/what-ive-learned-from-a-year-of-coding-bf39c5823e9b/',
         level:"complete beginner"
     },
     {
@@ -32,6 +34,31 @@ export const journeys = [
             'current ability, trying it, analyzing your performance while and after doing it, and correcting any ' +
             'mistakes. Make programming fun. Learn by doing.',
         link: 'https://www.norvig.com/21-days.html',
+        level:"complete beginner"
+    },
+    {
+        id: 'CB4',
+        title: 'Landing Your Dream Job 5 tips on becoming a self-taught programmer',
+        description: "This man took a 360 in his career by switching from doing an undergraduate in marketing followed " +
+            "by a master's in International Business in college. In the article, he gives 5 tips on how to start " +
+            "programming and what resources can help you along the way. Also, he explains what programming languages " +
+            "he learned first. He advanced from learning about HTML, CSS, and Javascript to more complex topics such " +
+            "as git, functional programming and object-oriented design.",
+        link: 'https://www.wunderlist.com/blog/Landing-your-dream-job/',
+        level:"complete beginner"
+    },
+    {
+        id: 'CB5',
+        title: 'Landing Your Dream Job 5 tips on becoming a self-taught programmer',
+        description: "Daniel K. Hunter learned to program while working at a 9-5 job. This is a great source of " +
+            "inspiration for anyone who is in school or has other commitments that make them believe they do not have " +
+            "the time to learn to program. Daniel explains that it took him 10 months and ~1400 hours of self-teaching " +
+            "to obtain a full-time developer job. Daniel claims he was never good at math, or science and did not even" +
+            " have a college degree however he managed to learn programing through discipline, motivation, inspiration, " +
+            "and courage. Also, Daniel provides a great list of resources he used to learn to code and explains " +
+            "what specifically he used each resource for. Furthermore, he provides a list of mistakes he made while " +
+            "learning so you won't repeat them." ,
+        link: 'https://www.freecodecamp.org/news/mornings-nights-and-weekends-how-i-changed-careers-and-became-a-programmer-197ce46ccc1c/',
         level:"complete beginner"
     },
     {
