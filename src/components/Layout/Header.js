770c3605ac1b64ab840de26346d04f1d8cbef881
Kristina Arezina <arezina.kristina23@gmail.com>
2019-10-13 16:29:59 -0400

    scrollable navbar works

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -3,11 +3,38 @@ import AppBar from '@material-ui/core/AppBar';
 import {Toolbar, Typography, Button} from '@material-ui/core';
 import TemporaryDrawer from "./SmallNavBar"
 import MediaQuery from 'react-responsive';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import PropTypes from 'prop-types';
+import Slide from '@material-ui/core/Slide';
 
 
-export class Header extends Component {
 
-    render(){
+function HideOnScroll(props) {
+    const { children, window } = props;
+    // Note that you normally won't need to set the window ref as useScrollTrigger
+    // will default to window.
+    // This is only being set here because the demo is in an iframe.
+    const trigger = useScrollTrigger({ target: window ? window() : undefined });
+
+    return (
+        <Slide appear={false} direction="down" in={!trigger}>
+            {children}
+        </Slide>
+    );
+}
+
+HideOnScroll.propTypes = {
+    children: PropTypes.element.isRequired,
+    /**
+     * Injected by the documentation to work in an iframe.
+     * You won't need it on your project.
+     */
+    window: PropTypes.func,
+};
+
+
+
+export default function Header (props) {
 
         return(
 
@@ -19,6 +46,7 @@ export class Header extends Component {
                 <MediaQuery query="(max-width: 599px)">
                 {matches =>
                     matches ? (
+                        <HideOnScroll {...props}>
                         <AppBar color="#f4f4f4">
                             <Toolbar>
                                 <Typography variant="h6" style={{flexGrow: 1}}>
@@ -30,8 +58,10 @@ export class Header extends Component {
                                     <TemporaryDrawer />
                             </Toolbar>
                         </AppBar>
+                        </HideOnScroll>
 
                     ) : (
+                        <HideOnScroll {...props}>
                         <AppBar color="#f4f4f4">
                             <Toolbar>
                                 <Typography variant="h6" style={{flexGrow: 1,}}>
@@ -66,14 +96,10 @@ export class Header extends Component {
                                     </Button>
                             </Toolbar>
                         </AppBar>
+                        </HideOnScroll>
 
                     )}
                 </MediaQuery>
             </div>
         )
-
-    }
 }
-
-
-export default Header;
