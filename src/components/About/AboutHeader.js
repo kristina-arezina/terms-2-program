7cf24ea40d63f28d63f3cece6e714bcc28e10589
Kristina Arezina <arezina.kristina23@gmail.com>
2019-10-22 16:16:21 -0400

    changed header to be nice when on phone

--- src/components/About/AboutHeader.js
+++ src/components/About/AboutHeader.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import MediaQuery from 'react-responsive';
 
 //everything for the header on the about page
 function AboutHeader(){
     return (
         <header >
-                <h1 style={headerStyle}>About Us</h1>
+            <MediaQuery query="(max-width: 841px)">
+                {matches =>
+                matches ? (
+                    <h1 style={headerStyle2}>About Us</h1>
+                ) :
+                    (
+                        <h1 style={headerStyle1}>About Us</h1>
+                    )}
+            </MediaQuery>
         </header>
     )
 }
 
-const headerStyle = {
+const headerStyle1 = {
     background:"#4db6ac",
     color:"#f4f4f4",
     textAlign: "left",
@@ -17,6 +26,20 @@ const headerStyle = {
     paddingTop:"150px",
     paddingBottom:"22px",
     fontSize:"60pt",
+    fontFamily:"century gothic",
+    fontWeight: "normal",
+    margin:"0px",
+    display:"Block",
+};
+
+const headerStyle2 = {
+    background:"#4db6ac",
+    color:"#f4f4f4",
+    textAlign: "left",
+    paddingLeft:"40px",
+    paddingTop:"100px",
+    paddingBottom:"22px",
+    fontSize:"40pt",
     fontFamily:"century gothic",
     fontWeight: "normal",
     margin:"0px",
