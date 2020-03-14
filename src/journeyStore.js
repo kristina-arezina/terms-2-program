76578e0d7b0ced57525bc6265169082953d036bd
Kristina Arezina <arezina.kristina23@gmail.com>
2020-03-14 10:30:22 -0400

    added a women in technology article

--- src/journeyStore.js
+++ src/journeyStore.js
@@ -84,12 +84,11 @@ export const journeys = [
     },
     {
         id: 'Women In Technology',
-        title: 'How I learned Python Programming RAPIDLY!',
+        title: 'The Secret History of Women in Coding',
         description: 'Chase Childers was programming for 3 years and is now a Full-Stack Python Developer/Programmer. ' +
-            'He started to code because he loves technology, creating and fixing things. Chase used Youtube, ' +
-            'the internet and a mentor to learn Python. He shares the challenges he faced while learning and gives ' +
-            'tips and advice to beginners.',
-        link: 'https://www.codementor.io/cchilders/how-i-learned-python-programming-rapidly-wqnklac0b',
+        'He started to code because he loves technology, creating and fixing things. Chase used Youtube, the internet and' +
+         'a mentor to learn Python. He shares the challenges he faced while learning and gives tips and advice to beginners.',
+        link: 'https://www.nytimes.com/2019/02/13/magazine/women-coding-computer-programming.html',
         level:"Women In Technology"
     },
 
