97e32d368e3c9fd1b40c678ea6a5059cc000850c
Kristina Arezina <arezina.kristina23@gmail.com>
2020-03-15 08:11:12 -0400

    the logic works for the womenInTech page

--- src/components/WomenInTech/Grid.js
+++ src/components/WomenInTech/Grid.js
@@ -34,61 +34,47 @@ const useStyles = makeStyles(theme => ({
 export default ({womenJourney}) => {
   const classes = useStyles();
 
-  return (
-    <div className={classes.root}>
-      <Grid container spacing={3}>
-        <Grid item xs={12} sm={6}>
-        <Paper style={{
-            backgroundColor:"#fffff",
-            padding:25,
-            fontFamily:"century gothic",
-        }}
-        >
-        {womenJourney.map(({ title, description, link, id }) =>
-            <ListItem alignItems="flex-start"  key={id}>
-                <Paper style={{
-                    backgroundColor:"#e0f2f1",
-                    padding:25,
-                    fontFamily:"century gothic",
-                }}>
-            <ListItemText
-                primary={
-                    <React.Fragment >
-                      {title}
-                    </React.Fragment>
-                }
-                secondary={
-                    <React.Fragment >
-                      {description}
-
-                                <br/>
-                                <Button className={classes.button}>
-                                    <Link className={classes.link} rel="noopener" variant="body1" href={link} target="_blank">
-                                        Read Full
-                                    </Link>
-                                </Button>
-                    </React.Fragment>
-                }>
-            </ListItemText>
+    return (
+      <div className={classes.root}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={6}>
+            <Paper style={{
+                backgroundColor:"#fffff",
+                padding:25,
+                fontFamily:"century gothic",
+            }}
+            >
+              {womenJourney.map(({ title, description, link, id }) =>
+                <ListItem alignItems="flex-start"  key={womenJourney}>
+                    <Paper style={{
+                        backgroundColor:"#fffff",
+                        padding:25,
+                        fontFamily:"century gothic",
+                    }}>
+                <ListItemText key={womenJourney}
+                    primary={
+                        <React.Fragment >
+                          {title}
+                        </React.Fragment>
+                    }
+                    secondary={
+                        <React.Fragment >
+                          {description}
+                    <br/>
+                          <Button className={classes.button}>
+                              <Link className={classes.link} rel="noopener" variant="body1" href={link} target="_blank">
+                                    Read Full
+                              </Link>
+                          </Button>
+                        </React.Fragment>
+                    }>
+              </ListItemText>
         </Paper>
-</ListItem>
-      //   <Grid item xs={12} sm={6}>
-      //     <Paper className={classes.paper}>xs=12 sm=6</Paper>
-      //   </Grid>
-      //     <Grid item xs={12} sm={6}>
-      //       <Paper className={classes.paper}>xs=12 sm=6</Paper>
-      //     </Grid>
-      //     <Grid item xs={12} sm={6}>
-      //       <Paper className={classes.paper}>xs=12 sm=6</Paper>
-      //     </Grid>
-      //     <Grid item xs={12} sm={6}>
-      //       <Paper className={classes.paper}>xs=12 sm=6</Paper>
-      //     </Grid>
-      //     <Grid item xs={12} sm={6}>
-      //       <Paper className={classes.paper}>xs=12 sm=6</Paper>
-      //     </Grid>
+        </ListItem>
 
-
-
-  ,
+      )}
+      </Paper>
+      </Grid>
+      </Grid>
+      </div>
 )}
